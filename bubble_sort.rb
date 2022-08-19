c85a8c8bcdcb1193de4f1d39bd4cbaf95397a29a
DanielT94 <daniel.turnbull94@gmail.com>
2022-08-19 11:53:20 +0100

    Ran rubocop for formatting

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,20 +1,20 @@
 def bubble_sort(arr)
-    unsorted = true
-    while unsorted do
-        i = 0
-        unsorted = false
-        while i < (arr.length - 1)
-            if arr[i] > arr[i + 1]
-                arr[i], arr[i + 1] = arr[i +1], arr[i]
-                unsorted = true
-            end
-            i += 1
-        end
+  unsorted = true
+  while unsorted
+    i = 0
+    unsorted = false
+    while i < (arr.length - 1)
+      if arr[i] > arr[i + 1]
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        unsorted = true
+      end
+      i += 1
     end
-    print arr
+  end
+  print arr
 end
 
-list = [4,3,78,2,0,2]
+list = [4, 3, 78, 2, 0, 2]
 
 bubble_sort(list)
-puts ""
+puts ''
